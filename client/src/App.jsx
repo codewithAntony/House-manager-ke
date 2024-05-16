e8fa36afe100d88b9b0c468adc52b85d6e5404e6
Antony Murithi <antonymurithi51@gmail.com>
2024-05-16 09:27:44 +0300

    create routes and paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,20 @@
-
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
 
 function App() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/profile" element={<Profile />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
