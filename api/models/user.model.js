b9e708d57017c2150199aa1b47d2476478f95b23
Antony Murithi <antonymurithi51@gmail.com>
2024-05-24 17:34:25 +0300

    complete google OAuth component

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -5,17 +5,22 @@ const userSchema = new mongoose.Schema(
         username: {
             type: String,
             required: true,
-            unique: true
+            unique: true,
         },
         email: {
             type: String,
             required: true,
-            unique: true
+            unique: true,
         },
         password: {
             type: String,
-            required: true
-        }
+            required: true,
+        },
+        avatar: {
+            type: String,
+            default:
+                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRv-XOuhizpTFq3b4bRFj9-RKeHeNUN-hX9BIL9LwCyoQ&s',
+        },
     },
     { timestamps: true }
 );
